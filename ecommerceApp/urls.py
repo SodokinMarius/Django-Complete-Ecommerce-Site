3f4e1ca118dddc2be024c7982c3b8288db0fae47
SodokinMarius <yaomariussodokin@gmail.com>
2022-10-20 13:02:11 +0100

    feat : add contact feature and inherit base file

--- ecommerceApp/urls.py
+++ ecommerceApp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import (
     AboutView,
     HomeView,
+    ContactView,
 )
 
 app_name="ecommerceApp"
@@ -10,4 +11,5 @@ app_name="ecommerceApp"
 urlpatterns = [
     path("",HomeView.as_view(),name="home"),
     path("about/",AboutView.as_view(),name="about"),
+    path("contact/",ContactView.as_view(),name="contact"),
 ]
